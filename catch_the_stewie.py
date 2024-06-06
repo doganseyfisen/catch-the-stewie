388117957fe61e26efa84ac9a21a9e76ea43edce
Dogan Seyfi Sen <doganseyfisen@gmail.com>
2024-06-06 17:58:06 +0300

    added new lines and changed some variables

--- catch_the_stewie.py
+++ catch_the_stewie.py
@@ -2,6 +2,7 @@ import turtle
 import random
 import time
 
+# Score and time display
 score_display = turtle.Turtle()
 score_display.hideturtle()
 score_display.penup()
@@ -14,20 +15,10 @@ time_display.goto(0, 350)
 
 # Score and time at beginning
 score = 0
-total_time = 1 * 60 # Minute
+total_time = 20 # Second
 start_time = time.time()
 
-# Update score and time
-def update_display():
-    score_display.clear()
-    time_display.clear()
-    score_display.write(f"Score: {score}", align="center", font=("Arial", 16, "bold"))
-    remaining_time = total_time - (time.time() - start_time)
-    remaining_min = int(remaining_time //  60)
-    remaining_sec = int(remaining_time %  60)
-    time_display.write(f"Remaining Time: {remaining_min}:{remaining_sec:02}", align="center", font=("Arial", 16, "bold"))
-
-# Screen
+# Game screen
 board = turtle.Screen()
 board.bgcolor('light green')
 board.title('Catch the Stewie')
@@ -36,8 +27,21 @@ board.register_shape('stewie.gif')
 # Stewie 
 stewie = turtle.Turtle()
 stewie.shape('stewie.gif')
-stewie.speed(8)
+stewie.speed(0)
 stewie.penup()
+
+# Update score and time
+def update_time():
+    time_display.clear()
+    # time.time() is current time
+    remaining_time = total_time - (time.time() - start_time)
+    remaining_min = int(remaining_time //  60)
+    remaining_sec = int(remaining_time %  60)
+    time_display.write(f"Remaining Time: {remaining_min}:{remaining_sec:02}", align="center", font=("Verdana", 16, "bold")) 
+
+def update_score():
+    score_display.clear()
+    score_display.write(f"Score: {score}", align="center", font=("Verdana", 16, "bold"))
 
 # Increase score
 def increase_score(x, y):
@@ -49,13 +53,18 @@ def increase_score(x, y):
     if x > stewie_x - 60 and x < stewie_x + 60 and y > stewie_y - 60 and y < stewie_y + 60:
         score += 10
         print(f"Congratulations! You earned {score} points.")
-        update_display()
+
+def start_game():
+    while time.time() - start_time < total_time:
+        update_score()
+        update_time()
+        x = random.randint(-240, 240)
+        y = random.randint(-240, 240)
+        stewie.setx(x)
+        stewie.sety(y)
+        time.sleep(1)
 
 board.onclick(increase_score)
 
-while time.time() - start_time < total_time:
-    x = random.randint(-240, 240)
-    y = random.randint(-240, 240)
-    stewie.setx(x)
-    stewie.sety(y)
-    time.sleep(1)
+start_game()
+turtle.mainloop()
